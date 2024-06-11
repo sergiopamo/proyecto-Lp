be393a0b32fbccda86ae0308381f14b3130da19a
Gaboootj10 <gabrielgotj@gmail.com>
2024-06-10 23:35:44 -0500

    Update Proyecto2.cpp

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,15 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 // Clase base Material
 class Material {
 private:
-    std::string titulo_;
-    std::string autor_;
+    string titulo_;
+    string autor_;
     int codigo_;
 public:
-    Material(const std::string& titulo, const std::string& autor, int codigo)
+    Material(const string& titulo, const string& autor, int codigo)
         : titulo_(titulo), autor_(autor), codigo_(codigo) {}
 
     virtual void mostrarDetalles() const {
-        std::cout << "Título: " << titulo_ << ", Autor: " << autor_ << ", Código: " << codigo_ << std::endl;
+        cout << "Título: " << titulo_ << ", Autor: " << autor_ << ", Código: " << codigo_ << endl;
     }
 
 };
@@ -17,14 +21,14 @@ public:
 // Clases derivadas
 class Libro : public Material {
 private:
-    std::string genero_;
+    string genero_;
 public:
-    Libro(const std::string& titulo, const std::string& autor, int codigo, const std::string& genero)
+    Libro(const string& titulo, const string& autor, int codigo, const string& genero)
         : Material(titulo, autor, codigo), genero_(genero) {}
 
     void mostrarDetalles() const override {
         Material::mostrarDetalles();
-        std::cout << "Género: " << genero_ << std::endl;
+        cout << "Género: " << genero_ << endl;
     }
 
 
@@ -34,12 +38,12 @@ class Revista : public Material {
 private:
     int numero_;
 public:
-    Revista(const std::string& titulo, const std::string& autor, int codigo, int numero)
+    Revista(const string& titulo, const string& autor, int codigo, int numero)
         : Material(titulo, autor, codigo), numero_(numero) {}
 
     void mostrarDetalles() const override {
         Material::mostrarDetalles();
-        std::cout << "Número: " << numero_ << std::endl;
+        cout << "Número: " << numero_ << endl;
     }
 
 
@@ -48,7 +52,7 @@ public:
 int main() {
     // Crea objetos de diferentes tipos de materiales
     Libro libro("Nombre del libro", "Autor del libro", 1001, "Genero");
-    Revista revista("Nombre de la Revista", "Auyores de la Revista", 2002, 123); // codigo - numero de pagina
+    Revista revista("Nombre de la Revista", "Autores de la Revista", 2002, 123); // codigo - numero de pagina
 
     // Crea un arreglo de punteros a Material
     Material* materiales[2];
@@ -58,7 +62,7 @@ int main() {
     // Muestra detalles utilizando polimorfismo
     for (const auto& material : materiales) {
         material->mostrarDetalles();
-        std::cout << "--------------------------" << std::endl;
+        cout << "--------------------------" << endl;
     }
 
     return 0;
