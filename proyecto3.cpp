f186ceb30d739f7cdb44bfbe837e224b2fe0a493
Spaighter <168687146+Spaighter@users.noreply.github.com>
2024-06-11 18:00:29 -0500

    Update proyecto3.cpp

--- proyecto3.cpp
+++ proyecto3.cpp
@@ -6,61 +6,61 @@ using namespace std;
 // Clase base Material
 class Material {
 private:
-  string titulo_;
-  string autor_;
-  int codigo_;
+  string titulo;
+  string autor;
+  int codigo;
 
 public:
   Material(const string& titulo, const string& autor, int codigo)
-      : titulo_(titulo), autor_(autor), codigo_(codigo) {}
+      : titulo(titulo), autor(autor), codigo(codigo) {}
 
   virtual void mostrarDetalles() const {
-    cout << "Título: " << titulo_ << ", Autor: " << autor_ << ", Código: " << codigo_ << endl;
+    cout << "Título: " << titulo << ", Autor: " << autor << ", Código: " << codigo << endl;
   }
 };
 
 // Clases derivadas
 class Libro : public Material {
 private:
-  string genero_;
+  string genero;
 
 public:
   Libro(const string& titulo, const string& autor, int codigo, const string& genero)
-      : Material(titulo, autor, codigo), genero_(genero) {}
+      : Material(titulo, autor, codigo), genero(genero) {}
 
   void mostrarDetalles() const override {
     Material::mostrarDetalles();
-    cout << "Género: " << genero_ << endl;
+    cout << "Género: " << genero << endl;
   }
 };
 
 class Revista : public Material {
 private:
-  int numero_;
+  int numero;
 
 public:
   Revista(const string& titulo, const string& autor, int codigo, int numero)
-      : Material(titulo, autor, codigo), numero_(numero) {}
+      : Material(titulo, autor, codigo), numero(numero) {}
 
   void mostrarDetalles() const override {
     Material::mostrarDetalles();
-    cout << "Número: " << numero_ << endl;
+    cout << "Número: " << numero << endl;
   }
 };
 
 class DVD : public Material {
 private:
-  int duracion_;
-  string director_;
+  int duracion;
+  string director;
 
 public:
   DVD(const string& titulo, const string& autor, int codigo, int duracion, const string& director)
-      : Material(titulo, autor, codigo), duracion_(duracion), director_(director) {}
+      : Material(titulo, autor, codigo), duracion(duracion), director(director) {}
 
   void mostrarDetalles() const override {
     Material::mostrarDetalles();
-    cout << "Duración: " << duracion_ << " minutos" << endl;
-    cout << "Director: " << director_ << endl;
+    cout << "Duración: " << duracion << " minutos" << endl;
+    cout << "Director: " << director << endl;
   }
 };
 
